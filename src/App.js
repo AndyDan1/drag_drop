026aba9b03fe16a7457e4ce1454d5667ddd29a3a
Andrii <itflenovo@gmail.com>
2022-10-03 16:41:22 +0300

    drag_drop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [boards, setBoards] = useState([
+    {
+      id: 1,
+      title: "Сделать",
+      items: [{id: Date.now() * Math.random(), title: 'Пойти в магазин'}, {id: Date.now()* Math.random(), title: 'Выкинуть мусор'}, {id: Date.now()* Math.random(), title: 'Купить молоко'}]
+    },
+    {
+      id: 2,
+      title: "Проверить",
+      items: [{id: Date.now() * Math.random(), title: 'Код ревью'}, {id: Date.now() * Math.random(), title: 'Задача на факториал'}, {id: Date.now() * Math.random(), title: 'новая таска'}]
+    },
+    {
+      id: 3,
+      title: "Сделано",
+      items: [{id: Date.now()* Math.random(), title: 'Почитать'}, {id: Date.now() * Math.random(), title: 'Английский'}, {id: Date.now() * Math.random(), title: 'Приготовить обед'}]
+    },
+  ])
+  const [currentBoard, setCurrentBoard] = useState(null)
+  const [currentItem, setCurrentItem] = useState(null)
+
+
+  const dragOverHandler = (e) => {
+    e.preventDefault()
+    if (e.target.className === 'item') {
+      e.target.style.boxShadow = '0 2px 3px gray'
+    }
+  };
+  const dragLeaveHandler = (e) => {
+    e.target.style.boxShadow = 'none'
+  };
+
+  const dragEndHandler = (e) => {
+    e.target.style.boxShadow = 'none'
+  };
+
+  const dragStartHandler = (e, board, item) => {
+    setCurrentBoard(board)
+    setCurrentItem(item)
+  };
+
+  const dropHandler = (e, board, item) => {
+    e.preventDefault()
+    const currentIndex = currentBoard.items.indexOf(currentItem)
+    currentBoard.items.splice(currentIndex, 1)
+
+    const dropIndex = board.items.indexOf(item)
+    board.items.splice(dropIndex+1, 0, currentItem)
+    setBoards(boards.map(b => {
+      if (b.id === board.id) {
+        return board
+      }
+      if (b.id === currentBoard.id) {
+        return currentBoard
+      }
+      return b
+    }))
+    e.target.style.boxShadow = 'none'
+  };
+
+
+
+  const dropCardHandler = (e, board) => {
+    board.items.push(currentItem)
+    const currentIndex = currentBoard.items.indexOf(currentItem)
+    currentBoard.items.splice(currentIndex, 1)
+
+    setBoards(boards.map(b => {
+      if (b.id === board.id) {
+        return board
+      }
+      if (b.id === currentBoard.id) {
+        return currentBoard
+      }
+      return b
+    }))
+    e.target.style.boxShadow = 'none'
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {boards.map(board => (
+        <div
+          className={'board'}
+          key={board.id}
+          onDragOver={(e) => dragOverHandler(e)}
+          onDrop={(e) => dropCardHandler(e, board)}
+
         >
-          Learn React
-        </a>
-      </header>
+          <div className={'board__title'} >{board.title}</div>
+          {board.items.map(item => (
+              <div
+                key={item.id}
+                onDragOver={(e) => dragOverHandler(e)}
+                onDragLeave={(e) => dragLeaveHandler(e)}
+                onDragStart={(e) => dragStartHandler(e, board, item)}
+                onDragEnd={(e) => dragEndHandler(e)}
+                onDrop={(e) => dropHandler(e, board, item)}
+                className={'item'}
+                draggable={true}
+              >
+                {item.title}
+              </div>
+            ))}
+        </div>
+      ))}
     </div>
   );
 }
